8b9ef4d6fa518adef22274c305c3aaa429026f78
lilei <lilei-3@thunisoft.com>
2020-08-25 11:30:12 +0800

    test

--- src/main/java/First/First.java
+++ src/main/java/First/First.java
@@ -1,18 +1,3 @@
-package First;/**
- *  First
- * @description TODO
- * @author lilei
- * @date 2020年08月25日 11:26:00
- * @version 1.0
- *//*
-* First
-* @description TODO
-* @param
-* @return
-* @author lilei
-* @date 2020/8/25 11:26
-* @Version 1.0
-* 
-* */
+package First;
 public class First {
 }
